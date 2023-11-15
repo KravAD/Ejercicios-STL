25eecf23a8f95dcf50a860b60f84b1334696050f
KravAD <dkrav@myuax.com>
2023-11-15 19:58:41 +0100

    commit 21
    Nuevo namespace mates en namespace Ciencia y nuevo include de cmath para operar con potencias.

--- Declaración de subespacios de nombres/Ciencia.cpp
+++ Declaración de subespacios de nombres/Ciencia.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <cmath>
 namespace Ciencia {
     namespace Fisica {
         const double c = 299792458;
         double calcularEnergia(double masa){
             return masa + c * c;
+        }
+    }
+    namespace Mates {
+        double calcularpotencia(double base, double exponente){
+            return pow(base, exponente);
         }
     }
 }
