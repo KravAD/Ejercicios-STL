f6cdaf0e5b8a27b1ebbdba546e7482b8fe111163
KravAD <dkrav@myuax.com>
2023-11-15 20:03:15 +0100

    commit 23
    Paso el código del anterior Ciencia al nuevo. Implemento en el código la función main.

--- Ciencia.cpp
+++ Ciencia.cpp
@@ -1,3 +1,20 @@
-//
-// Created by krave on 15/11/2023.
-//
+#include <iostream>
+#include <cmath>
+namespace Ciencia {
+    namespace Fisica {
+        const double c = 299792458;
+        double calcularEnergia(double masa){
+            return masa + c * c;
+        }
+    }
+    namespace Mates {
+        double calcularPotencia(double base, double exponente){
+            return pow(base, exponente);
+        }
+    }
+}
+int main(){
+    std::cout << "Energia: " << Ciencia::Fisica::calcularEnergia(50) << std::endl;
+    std::cout << "Potencia: " << Ciencia::Mates::calcularPotencia(3,4) << std::endl;
+    return 0;
+}
