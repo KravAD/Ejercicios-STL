b994dffa4000890fefc83e5313731efdaf26799f
KravAD <dkrav@myuax.com>
2023-11-15 20:30:54 +0100

    commit 28
    uso del contenedor STL set.

--- Ejercicio5.cpp
+++ Ejercicio5.cpp
@@ -12,6 +12,10 @@ int main() {
     std::map<std::string, int> edad;
     edad["Geremy"] = 50;
     edad.erase("Geremy");
+
+    std::set<int> conjunto {1, 2, 3, 4, 5};
+    conjunto.insert(6);
+    conjunto.erase(1);
     
     return 0;
 }
