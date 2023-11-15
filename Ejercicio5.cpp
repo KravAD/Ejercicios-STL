6b423fc36a991cad1139d484ebdd456f10f5cdff
KravAD <dkrav@myuax.com>
2023-11-15 20:34:49 +0100

    commit 29
    cout en el codigo de set.

--- Ejercicio5.cpp
+++ Ejercicio5.cpp
@@ -12,10 +12,12 @@ int main() {
     std::map<std::string, int> edad;
     edad["Geremy"] = 50;
     edad.erase("Geremy");
+    std::cout << edad.size() << std::endl;
 
     std::set<int> conjunto {1, 2, 3, 4, 5};
     conjunto.insert(6);
     conjunto.erase(1);
-    
+    std::cout << conjunto.size() << std::endl;
+
     return 0;
 }
