87e2d0f4346620381d84761a6487b43c5bc33268
KravAD <dkrav@myuax.com>
2023-11-15 20:30:19 +0100

    commit 27
    Uso contenedor STL map.

--- Ejercicio5.cpp
+++ Ejercicio5.cpp
@@ -8,6 +8,10 @@ int main() {
 
     std::vector<int> numeros {1, 2, 3, 4, 5};
     numeros.push_back(6);
+
+    std::map<std::string, int> edad;
+    edad["Geremy"] = 50;
+    edad.erase("Geremy");
     
     return 0;
 }
