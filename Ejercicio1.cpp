26120fd29771f05283c9243ed1ab570dc428e616
KravAD <dkrav@myuax.com>
2023-11-15 18:15:25 +0100

    commit 7
    Llamada función división.

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -26,11 +26,12 @@ int main(){
     int suma = matematicas::suma(1, 2);
     int resta = matematicas::resta(20, 13);
     int multiplicacion = matematicas::multiplicacion(2, 5);
+    int division = matematicas::division(6, 2);
 
     std::cout << "La suma es: " << suma << std::endl;
     std::cout << "La resta es: " << resta << std::endl;
     std::cout << "La multiplicacion es: " << multiplicacion << std::endl;
-    
+    std::cout << "La division es: " << division << std::endl;
 
     return 0;
 }
